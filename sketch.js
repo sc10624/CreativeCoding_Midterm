8e7b80cd060edb713ac8c612043f2cbbf4963181
sc10624 <157539932+sc10624@users.noreply.github.com>
2024-04-08 23:48:20 -0400

    worked on scene 2
    Made the object move from one bush to another & stay there

--- sketch.js
+++ sketch.js
@@ -20,7 +20,7 @@ let mini; // a creature
 
 // scene two object
 let bushes = [];
-let grass = [];
+//let grass = [];
 /*
 let bush1;
 let bush2;
@@ -28,6 +28,7 @@ let bush3;
 */
 //let grass1;
 let miniTwo;
+let move = false;
 
 //scene three object
 let threeObjects = [];
@@ -37,8 +38,10 @@ let threeB;
 */
 
 // Creature position
+/*
 let x;
 let y;
+*/
 
 // ((CHANGED THE SCOPE OF THESE VARIABLES BELOW))
 //let hover;
@@ -58,8 +61,10 @@ function setup() {
 
   c = orange;
 
+  /*
   x = 400;
   y = 400;
+  */
 
   //------------scene 1--------------
   mini = new Creature(400,400);
@@ -67,25 +72,30 @@ function setup() {
   //------------scene 2---------------
   // fixed y, random x
   //let bushX = 100;
+  /*
   let bushY = 125;
   for (let i = 0; i < 3; i++){
     bushes[i] = new Bushes(random(100,width-300),bushY); //** MAKE THE SPACING EVEN
     bushY += 300;
   } 
+  */
 
+  /*
   let grassX = 0;
   let grassY = 50;
   for (let i = 0; i < 100; i++){
     grass[i] = new Grass(grassX,grassY)
     grassX += 50;
   }
+  */
+
   /*
   bush1 = new Bushes(550,200);
   bush2 = new Bushes(100,400);
   bush3 = new Bushes(550,700);
   grass1 = new Grass(50,50);
   */
-  miniTwo = new Creature(500,600);
+  miniTwo = new Creature(100,125);
 
   //------------scene 3--------------
   for (let i = 0; i < 10; i++){
@@ -121,10 +131,10 @@ function draw() {
   
   mini.blink();
   mini.sideJump();
-  //mini.checkHover();
+  mini.checkHover();
   //mini.checkSmile();
   mini.smileIfHover();
-  mini.tickle();
+  mini.mouseTickle();
   mini.display();
 } // option 1 ends
   
@@ -144,22 +154,40 @@ if (option == 2){
   //**PLANT THE GRASS ALL OVER THE SCREEN USING ARRAY**
   //grass1.display();
 
+  /*
   for(i = 0; i < 50; i++){
     grass[i].display();
   }
+  */
 
-  miniTwo.hide(bushes[0]);
+  for(let x = 0; x < width; x += 50){
+    for(let y = 50; y < height; y += 50){
+      grass(x,y);
+    }
+  }
+
+  //miniTwo.hide(bushes[0]);
+  //miniTwo.hide(100,125);
+
+
+  //setTimeout(moveHide(500,425),3000);
+  miniTwo.moveHide(500,425);
   miniTwo.blink();
-  miniTwo.walk(); // **MAKE THE OBJECT MOVE FROM ONE BUSH TO ANOTHER, THEN STOP AT THAT LOCATION FOR A FEW SECONDS**
+  //miniTwo.walk(); // **MAKE THE OBJECT MOVE FROM ONE BUSH TO ANOTHER
+  miniTwo.checkHover();
   miniTwo.smileIfHover();
-  miniTwo.tickle();
+  miniTwo.mouseTickle();
   miniTwo.display();
 
 
+  bush(100, 125);
+  bush(500, 425);
+  bush(100, 725);
+  /*
   for(i = 0; i < 3; i++){
     bushes[i].display();
   }
-
+  */
   
 
   /*
@@ -184,9 +212,10 @@ if (option == 3){
 
   for(let i = 0; i < 10; i ++){
     threeObjects[i].blink();
+    threeObjects[i].checkHover();
     threeObjects[i].smileIfHover();
     threeObjects[i].wander();
-    threeObjects[i].tickle();
+    threeObjects[i].mouseTickle();
     // ** FIGURE OUT HOW TO USE giggleIfNear FUNCTION WITH NOT JUST ONE OBJECT BUT EVERY OTHER OBJECT
     threeObjects[i].giggleIfNearMany();
     threeObjects[i].display();
@@ -196,14 +225,14 @@ if (option == 3){
   threeA.blink();
   threeA.smileIfHover();
   threeA.walk();
-  threeA.tickle();
+  threeA.mouseTickle();
   threeA.giggleIfNear(threeB);
   threeA.display();
 
   threeB.blink();
   threeB.smileIfHover();
   threeB.walk();
-  threeB.tickle();
+  threeB.mouseTickle();
   threeB.giggleIfNear(threeA);
   threeB.display();
   */
@@ -234,56 +263,102 @@ constructor(x,y){
   let closed = false;
   this.closed = closed;
 
-  let direction = random([-1,1]);
-  this.direction = direction;
-
-  let speed = random(1,5);
+  let xDirection = random([-1,1]);
+  this.xDirection = xDirection;
+  let yDirection = random([-1,1]);
+  this.yDirection = yDirection;
+  let speed = random(1,3);
   this.speed = speed;
+
+  /*
+  let move = false;
+  this.move = move;
+  */
 }
 
 
 walk(){ // walk within the screen
   this.speed = 2; // fixed the speed bc it's walk
 
-  this.x = this.x + (this.direction*this.speed);
+  this.x = this.x + (this.xDirection*this.speed);
   this.y = 10*sin(frameCount*0.3) + this.initialY;
 
   if(this.x > width || this.x < 0){
-    this.direction *= -1;
+    this.xDirection *= -1;
   }
   /*
   if (this.x < 0){
-    this.direction *= 1;
+    this.xDirection *= 1;
   }
   */
 } // walk ends
 
 
-wander(){ // ** MAKE Y DIRECTION CHANGE TOO
+wander(){
   //this.speed = 2; // let the speed be random bc it's wander
 
-  this.x = this.x + (this.direction*this.speed);
+  this.x = this.x + (this.xDirection*this.speed);
   this.y = 10*sin(frameCount*0.3) + this.initialY;
+  this.initialY = this.initialY + (this.yDirection*this.speed);
 
   if(this.x > width){
-    this.direction = -1;
+    this.xDirection = -1;
+    this.speed = random(1,3);
   }
   if (this.x < 0){
-    this.direction = 1;
+    this.xDirection = 1;
+    this.speed = random(1,3);
+  }
+  if(this.y > height){
+    this.yDirection = -1;
+    this.speed = random(1,3);
+  }
+  if(this.y < 0){
+    this.yDirection = 1;
+    this.speed = random(1,3);
   }
 } // wander ends
 
 
+hide(x, y){  
+  this.x = x;
+  this.y = y;
+  //print(this.initialX);
+  //print("object: " + object.y);
+  //print("initial: " + this.initialY);
+} // hide ends
+
+
+moveHide(x,y){
+  /*
+  this.x = x;
+  this.y = y;
+  */
+ if(this.x != x || this.y != y){
+  this.x += (x - this.initialX)/100;
+  this.y += (y - this.initialY)/100;
+  //this.y = 10*sin(frameCount*0.6) + this.y; //** tried to make it look more like walking but x and y doesn't stop at the exact location
+ }
+ if(this.x >= x || this.y >= y){ //** the x and y need to be the exact location in order to stop...
+    this.x = this.x;
+    this.y = this.y;
+  }
+}
+
+
+
+/*
 //***************************************IN PROGRESS*******************************************
 hide(object){  
   this.initialX = object.x;
   this.initialY = object.y;
 
-  //print(initialX);
-
+  //print(this.initialX);
+  //print("object: " + object.y);
+  //print("initial: " + this.initialY);
 } // hide ends
 //*********************************************************************************************
-
+*/
 
 sideJump(){
   this.x = 70*cos(frameCount*0.1) + this.initialX;
@@ -291,7 +366,7 @@ sideJump(){
 } // sideJump ends
 
 
-tickle(){ // ** make the character laugh and shake its limbs when tickled -- need a boolean variable that indicates whether it is being tickled or not
+mouseTickle(){ // ** make the character laugh and shake its limbs when tickled -- need a boolean variable that indicates whether it is being tickled or not
   if(mouseIsPressed){
     if(this.hover){
       if(option == 1){
@@ -316,43 +391,9 @@ tickle(){ // ** make the character laugh and shake its limbs when tickled -- nee
 } // tickle ends
 
 
-/*
-tickle(){
-  //this.x = this.x + random(-10,10);
-}
-*/
-
-/*
-smileYes(){
-  smile = true;
-}
-smileNo(){
-  smile = false;
-}
-*/
-
-/*
-eyesClosed(){
-  closed = true;
-}
-eyesNotClosed(){
-  closed = false;
-}
-*/
-
-
 // referenced a coding train video provided on github
-///*
 giggleIfNear(other){ // creatures smile when they near each other
-  // ** HOW DO I CHECK WHEN THE OBJECTS ARE CLOSE TO EACH OTHER? -- for statement going through the array
 
-  //***************************************************************
-
-  // COME BACK TO THIS ONE LATER TODAY!!!!!!! MUST BE FIGURED OUT BY TODAY OR ELSE I DIE
-
-  //***************************************************************
-
-  //for (let i = 0; i < threeObjects.length; i ++){ // for statement checking the distance and hover status of objects
   let distance = dist(this.x,this.y,other.x,other.y);
   if (distance < 25*2){
     this.smile = true;
@@ -366,15 +407,10 @@ giggleIfNear(other){ // creatures smile when they near each other
     line(this.x+45, this.y-30, this.x+random(55,60), this.y-random(35,40));
     line(this.x+35, this.y-30, this.x+random(40,45), this.y-random(40,45));
   } // if statement ends
-  //} // for statement ends
 } // giggleIfNear ends
-//*/
 
 
 giggleIfNearMany(){ // creatures smile and giggle when they near each other
-  //***************************************************************
-  // DOESN'T WORK
-  //***************************************************************
   /*
   for (let i = 0; i < threeObjects.length; i ++){ // for statement checking the distance and hover status of objects
     if (this != threeObjects[i]){
@@ -405,18 +441,29 @@ giggleIfNearMany(){ // creatures smile and giggle when they near each other
 } // giggleIfNear ends
 
 
-
-smileIfHover(){
+checkHover() {
   let distance = dist(mouseX,mouseY,this.x,this.y);
-  //print(distance);
-
-  // check hover
-  // hover is true when mouse is near the character
   if (distance < 25*2.5){ // giving more room for error (bc the figure moves too fast -- need to slow it down)
     this.hover = true;
   } else{
     this.hover = false;
   }
+}
+
+
+smileIfHover(){
+  //let distance = dist(mouseX,mouseY,this.x,this.y);
+  //print(distance);
+
+  // check hover
+  // hover is true when mouse is near the character
+  /*
+  if (distance < 25*2.5){ // giving more room for error (bc the figure moves too fast -- need to slow it down)
+    this.hover = true;
+  } else{
+    this.hover = false;
+  }
+  */
 
   // smile is true when hover is true
   if (this.hover == true){
@@ -507,7 +554,26 @@ if(this.smile == true){
 
 
 
+function bush(x,y){
+  stroke(0);
+  strokeWeight(10);
+  fill(darkGreen);
 
+  ellipse(x,y-5,90,90);
+  ellipse(x+50,y,90,90);
+  ellipse(x+100,y-5,90,90);
+  ellipse(x+25,y-40,90,90);
+  ellipse(x+75,y-40,90,90);
+  noStroke();
+  ellipse(x,y-5,90,90);
+  ellipse(x+50,y,90,90);
+  ellipse(x+100,y-5,90,90);
+  ellipse(x+25,y-40,90,90);
+  ellipse(x+75,y-40,90,90);
+}
+
+
+/*
 class Bushes {
 
   constructor(x,y){
@@ -533,7 +599,19 @@ class Bushes {
     ellipse(this.x+75,this.y-40,90,90);
   } // display ends
 } // Bushes ends
+*/
 
+
+function grass(x,y){
+  noStroke();
+  fill(lightGreen);
+  triangle(x,y-15,x-15,y+15,x+15,y+15);
+
+  print(x);
+}
+
+
+/*
 class Grass{
   constructor(x,y){
     this.x = x;
@@ -546,7 +624,7 @@ class Grass{
   triangle(this.x,this.y-15,this.x-15,this.y+15,this.x+15,this.y+15);
   } // display ends
 } // Grass ends
-
+*/
 
 
 
@@ -559,119 +637,4 @@ function keyPressed() {
     option = 1;
   }
 
-}
-
-
-
-
-
-
-
-//**************************************************
-// an atempt to make a moving ball
-//**************************************************
-
-
-
-
-
-// BALL DOESN'T SHOW UP?
-/*
-let mini;
-
-function setup() {
- // put setup code here
-  createCanvas(800,800);
-  mini = new Ball(400,400);
-}
-
-function draw() {
-  // put drawing code here
-  background(255);
-  mini.display;
-}
-
-class Ball {
-
-  constructor(x,y) {
-    this.x = x;
-    this.y = y;
-  }
-
-  display() {
-  stroke(0);
-  strokeWeight(3);
-  fill(124);
-  ellipse(this.x,this.y,50,50);
-  }
-}
-*/
-
-
-//----------------------------------------------------------------------------------------
-// using code from class for truobleshooting (figuring out why the ball doesn't display with my code)
-//----------------------------------------------------------------------------------------
-
-
-
-// let mini;
-
-
-// function setup() {
-//   createCanvas(800, 800)
-//  v = createVector(width/2, 48);
-//  mini = new Ball(v);
-// }
-
-// function draw() {
-
-//   background(0);
-//   mini.display();
-// }
-
-// // class definition
-// /*
-// class Ball{
-
-// constructor(x, y){
-//   this.x = x;
-//   this.y = y;
-// }
-
-// display(){
-//   stroke(127);
-//   strokeWeight(2);
-//   fill(255, 127);
-//   ellipse(this.x, this.y, 48, 48)
-// }
-
-// }
-// */
-
-// class Ball{
-
-//     constructor(position){
-//         /*
-//       this.x = x;
-//       this.y = y;
-//       */
-//       this.position = position;
-//     }
-    
-//     /*
-//     update(){ //update object variables associated with this particular object
-//         this.velocity.add(this.acceleration); //add this particular object's acceleration to the velocity
-//         this.position.add(this.velocity); // add velocity to this particular object's location (thus moving the object)
-//         this.lifespan -= 2; // decrease the lifespan by 2 each time
-//     }
-//     */
-
-//     display(){
-//       stroke(127);
-//       strokeWeight(2);
-//       fill(255);
-//       ellipse(this.position.x, this.position.y, 48, 48) // beacuse position contains x and y locations, you need to pull out x and y respectively!
-//     }
-    
-    
-//     }
+} // keyPressed ends
